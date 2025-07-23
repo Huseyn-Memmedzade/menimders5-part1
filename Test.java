92f1ae7cb1d3c042801a5565aa92ed0cd5284bd4
Backend Developer <hseyn.mmmdzad@bk.ru>
2025-07-23 11:29:10 +0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,26 @@
+package yenipabka;
+
+public class Test {
+    public static void main(String[] args) {
+
+        int[] ededler = new int[]{3, 7, 1, 5, 9};
+        int cem = 0;
+        for (int i = 0; i < ededler.length; i++) {
+            cem = cem + ededler[i];
+        }
+        System.out.println(cem);
+    }
+}
+
+// ✅ 1. Ədədlərin cəmini tap
+// 5 elementli bir tam ədəd massivindəki bütün ədədlərin cəmini hesabla.
+// Nümunə massiv: {3, 7, 1, 5, 9}
+
+
+
+
+
+
+
+
+
